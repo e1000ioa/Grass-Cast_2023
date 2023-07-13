7252d43798a654138a40401b38885961aae3944d
Emilio Aguilar <61033492+e1000ioa@users.noreply.github.com>
2023-07-13 15:49:57 -0700

    plot_combination_finished

---
+++ Thre_Graph_Line.R
@@ -0,0 +1,109 @@
+#libraries
+library(ggplot2)
+library(dplyr)
+library(ggrepel)
+library(ggpubr)
+library(ggtext) #for shaowtext 
+
+
+
+###### ENSO ####
+
+### Prep data Set ####
+
+#Data
+
+# Create a data frame with JMF 
+ENSO_SP <- read.csv("data/ENSOvsSpr1980_2020.csv") %>% filter(year >= 1980)
+xj <- unlist(ENSO_SP[4]) #JMF
+yj <- unlist(ENSO_SP[15])
+zj <- unlist(ENSO_SP[3])
+JMF <- data.frame(x=xj, y=yj, year = z)
+model_JMF <- lm(x ~ y, data=JMF)
+summary(model_JMF)
+
+# Create a data frame with MAM
+ENSO_SU <- read.csv("data/ENSOvsSU_1950_2020.csv") %>% filter(Year >= 1980)
+colnames(ENSO_SU)
+xm <- unlist(ENSO_SU[4])
+ym <- unlist(ENSO_SU[8])
+zm <- unlist(ENSO_SU[3])
+MAM <- data.frame(x=xm, y=ym, year = z)
+model_MAM <- lm(x ~ y, data=MAM)
+summary(model_MAM)
+
+### Create a scatter plot with linear JMF #####
+J <- ggplot(JMF, aes(x = x, y = y,label=JMF$year)) +
+  geom_point(color = "#AAB645", alpha = 1) +
+  geom_smooth(method="lm", formula=y~x, se=FALSE, color="#AAB645") +
+  geom_text_repel(size=3,alpha = 0.3, nudge_y=-(max(y)/100))+
+  xlab(paste("JMF (°C)")) +
+  ylab(paste("ANPP mean anommally  (lb/ac)")) +
+  
+  geom_hline(yintercept=0, linetype="dashed", color="gray") +
+  geom_vline(xintercept=0, linetype="dashed", color="gray") +
+  
+  annotate(
+    "text",
+    x = -1.10,
+    y = 40,
+    label = paste(expression("R²"), "=", signif(summary(model_JMF)$r.squared, digits = 3)),
+    color = "#595959",
+    size = 5
+  ) + #Annotate R2
+  
+  
+  annotate(
+    "text",
+    x = -1.1,
+    y = 45,
+    label = paste0("y = ", round(coef(model_JMF)[2], digits = 2), "x ", signif(coef(model_JMF)[1], digits = 3)),
+    color = "#595959",
+    size = 5
+  ) + #Annotate linear equation
+  
+  theme_classic() +
+  theme(axis.line = element_blank(),
+    panel.border = element_rect(colour = "black", fill = NA, size=0.8))
+
+### Create a scatter plot with linear MAM #####
+M <- ggplot(MAM, aes(x = x, y = y,label=MAM$year)) +
+  geom_point(color = "#FEC306", alpha = 1) +
+  geom_smooth(method="lm", formula=y~x, se=FALSE, color="#FEC306") +
+  geom_text_repel(size=3,alpha = 0.3, nudge_y=-(max(y)/100))+
+  xlab(paste("MAM (°C)")) +
+  ylab(paste("ANPP mean anommally  (lb/ac)")) +
+  
+  geom_hline(yintercept=0, linetype="dashed", color="gray") +
+  geom_vline(xintercept=0, linetype="dashed", color="gray") +
+  
+  annotate(
+    "text",
+    x = -0.7,
+    y = 40,
+    label = paste(expression("R²"), "=", signif(summary(model_MAM)$r.squared, digits = 3)),
+    color = "#595959",
+    size = 5
+  ) + #Annotate R2
+  
+  annotate(
+    "text",
+    x = -0.7,
+    y = 45,
+    label = paste0("y = ", round(coef(model_MAM)[2], digits = 2), "x ", signif(coef(model_MAM)[1], digits = 3)),
+    color = "#595959",
+    size = 5
+  ) + #Annotate linear equation
+  
+  theme_classic() +
+  theme(axis.line = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=0.8))
+
+
+#### Combine multiple ggplot on one page #### 
+
+ggarrange(J, M,
+          labels = c("A", "B"),
+          ncol = 2, nrow = 1)
+
+ggsave("images/JMF_MAM_ENSO.png", dpi = 350)
