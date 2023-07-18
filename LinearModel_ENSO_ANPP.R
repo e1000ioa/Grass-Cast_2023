dafcc59007de62ee8df8b54b2b5810c2a3fc1f13
Emilio Aguilar <61033492+e1000ioa@users.noreply.github.com>
2023-07-18 09:51:32 -0700

    ONly one repo

--- LinearModel_ENSO_ANPP.R
+++ LinearModel_ENSO_ANPP.R
@@ -1,11 +1,12 @@
 #libraries
 library(ggplot2)
 library(dplyr)
-library(ggrepel)
+library(ggrepel) #for year labels
 library(ggpubr)
-library(ggtext) #for shaowtext 
+library(finalfit) #Pvalue format
 
-###### ENSO ####
+
+  ###### ENSO ####
 
 ### Prep data Set ####
 
@@ -17,8 +18,13 @@ xj <- unlist(ENSO_SP[4]) #JMF
 yj <- unlist(ENSO_SP[15])
 zj <- unlist(ENSO_SP[3])
 JFM <- data.frame(x=xj, y=yj, year = zj)
-model_JFM <- lm(xj ~ yj, data=JFM)
-summary(model_JFM)
+
+model_JFM <- lm(x ~ y, data=JFM)
+summary_model_JFM <- summary(model_JFM)
+p_value <- summary_model_JFM$coefficients[2, "Pr(>|t|)"]
+
+finalfit::p_tidy(p_value,
+                 digits = 3)
 
 ### Create a scatter plot with linear JMF #####
 J <- ggplot(JFM, aes(x = JFM$x, y = JFM$y, label=JFM$year)) +
@@ -34,34 +40,40 @@ J <- ggplot(JFM, aes(x = JFM$x, y = JFM$y, label=JFM$year)) +
   ylim(-40,45) +
   xlim(-1.5,1.5) +
   
+  #Annotate linear equation
   annotate(
     "text",
-    x = -1.10,
-    y = 40,
-    label = paste(expression("R²"), "=", signif(summary(model_JFM)$r.squared, digits = 3)),
-    color = "#595959",
-    size = 5
-  ) + #Annotate R2
-  
-  
-  annotate(
-    "text",
-    x = -1.1,
+    x = -1.4,
     y = 45,
     label = paste0("y = ", round(coef(model_JFM)[2], digits = 2), "x ", signif(coef(model_JFM)[1], digits = 3)),
     color = "#595959",
-    size = 5
-  ) + #Annotate linear equation
+    size = 5,
+    hjust = 0
+  ) + 
   
   
+  #Annotate R2
   annotate(
     "text",
-    x = -1.10,
-    y = 35,
-    label = paste("p value =", signif(summary(model_JFM)$coefficients[2,4], digits = 2)),
+    x  = -1.4,
+    y = 40,
+    label = paste(expression("R²"), "=", signif(summary(model_JFM)$r.squared, digits = 3)),
     color = "#595959",
-    size = 5
-  ) + #Annotate p value
+    size = 5,
+    hjust = 0
+  ) + 
+  
+
+  #Annotate p value
+  annotate(
+    "text",
+    x  = -1.4,
+    y = 35,
+    label = paste("p", finalfit::p_tidy(summary(model_JFM)$coefficients[2,4], digits = 2)),
+    color = "#595959",
+    size = 5,
+    hjust = 0
+  ) + 
   
   theme_classic() +
   theme(axis.line = element_blank(),
@@ -72,11 +84,11 @@ ENSO_SU <- read.csv("data/ENSOvsSU_1950_2020.csv") %>% filter(Year >= 1980)
 colnames(ENSO_SU)
 xm <- unlist(ENSO_SU[4])
 ym <- unlist(ENSO_SU[8])
-zm <- unlist(ENSO_SU[3])
+zm <- unlist(ENSO_SU[2])
 MAM <- data.frame(x=xm, y=ym, year = zm)
+
 model_MAM <- lm(x ~ y, data=MAM)
 summary_model_MAM <- summary(model_MAM)
-
 p_value <- summary_model_MAM$coefficients[2, "Pr(>|t|)"]
 
 ### Create a scatter plot with linear MAM #####
@@ -95,35 +107,40 @@ M <- ggplot(MAM, aes(x = x, y = y,label=MAM$year)) +
   
   annotate(
     "text",
-    x = -1.10,
+    x = -1.4,
     y = 40,
     label = paste(expression("R²"), "=", signif(summary(model_MAM)$r.squared, digits = 3)),
     color = "#595959",
-    size = 5
+    size = 5,
+    hjust = 0
   ) + #Annotate R2
   
   annotate(
     "text",
-    x = -1.1,
+    x = -1.4,
     y = 45,
     label = paste0("y = ", round(coef(model_MAM)[2], digits = 2), "x ", signif(coef(model_MAM)[1], digits = 3)),
     color = "#595959",
-    size = 5
+    size = 5,
+    hjust = 0
   ) + #Annotate linear equation
   
   annotate(
     "text",
-    x = -1.10,
+    x = -1.4,
     y = 35,
-    label = paste("p value =", signif(summary(model_MAM)$coefficients[2,4], digits = 2)),
+    label = paste("p", finalfit::p_tidy(summary(model_MAM)$coefficients[2,4], digits = 2)),
     color = "#595959",
-    size = 5
+    size = 5,
+    hjust = 0
   ) + #Annotate R2
   
   theme_classic() +
   theme(axis.line = element_blank(),
         panel.border = element_rect(colour = "black", fill=NA, size=0.8))
 
+
+M
 #### Combine multiple ggplot on one page #### 
 
 ggarrange(J, M,
@@ -136,4 +153,3 @@ ggsave("images/NDVI_ANPP_lm.png",
        width = 33,
        units = "cm")
 
-ggsave("images/JMF_MAM_ENSO.png", dpi = 350)
