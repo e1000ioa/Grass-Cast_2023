07307575a9fd4dfbd3004b07a850eef26371d570
Emilio Aguilar <61033492+e1000ioa@users.noreply.github.com>
2023-08-18 20:39:13 -0700

    FInal usa commit 2

--- LinearModel_iNDVI_HarvestedANPP.R
+++ LinearModel_iNDVI_HarvestedANPP.R
@@ -1,63 +1,87 @@
 # Load the required libraries
 library(ggplot2)
 library(ggpubr)
+library(dplyr)
 
 # Read the data from the file
-NPPiNDVI <- read.csv("data/NPPNDVI_SW_sites.csv")
+NPPiNDVI <- read.csv("data/NPPNDVI_SW_sites.csv")  %>%
+  mutate(SITE = ifelse(SITE == "TORREL", "JORNADA", SITE)) %>%
+  filter(ALL == "YES")
 
 # Create a scatter plot
 
-ploter <- function(data){
+custom_colors <- c("#AAB645", "#DF5327", "#276EDF")
+
+model_yearly <- lm(ANPP ~ NDVI, data=NPPiNDVI)
+
+#Calculate RSME
+# Calculate residuals
+residuals <- NPPiNDVI$ANPP - NPPiNDVI$NDVI
+
+# Calculate RMSE
+rmse <- sqrt(mean(residuals^2))
+
+# Calculate residuals
+residuals <- y_observed - y_predicted
+
+# Calculate RMSE
+rmse <- sqrt(mean(residuals^2))
+
+ ggplot(NPPiNDVI, aes(x = NDVI, y = ANPP)) +
+  geom_point(aes(color = SITE)) +
+  scale_color_manual(values = custom_colors) + 
+  xlab("iNDVI") +
+  ylab("ANPP (lb/acre)") +
+  geom_smooth(method="lm", formula=y~x, se=FALSE, color="#595959") +
   
-  custom_colors <- c("#AAB645", "#DF5327", "#FEC306") #"#276EDF")
+  ylim(0, 425) +  # Set x limits
+  xlim(0.1, 0.45) +  # Set y limits +
   
-  model_yearly <- lm(ANPP ~ NDVI, data=data)
   
-  plot <- ggplot(data, aes(x = NDVI, y = ANPP)) +
-    geom_point(aes(color = SITE)) +
-    scale_color_manual(values = custom_colors) + 
-    xlab("iNDVI") +
-    ylab("ANPP (lb/acre)") +
-    geom_smooth(method="lm", formula=y~x, se=FALSE, color="#595959") +
-    
-    ylim(0, 425) +  # Set x limits
-    xlim(0.1, 0.45) +  # Set y limits +
-    
-    
-    annotate(
-      "text",
-      y = 365,
-      x = 0.15,
-      label = paste(expression("R²"), "=", signif(summary(model_yearly)$r.squared, digits = 3)),
-      color = "#595959",
-      size = 5
-    ) + #Annotate R2
-    
-    
-    annotate(
-      "text",
-      y = 400,
-      x = 0.17,
-      label = paste0("y = ", round(coef(model_yearly)[2], digits = 2), "x ", signif(coef(model_yearly)[1], digits = 3)),
-      color = "#595959",
-      size = 5
-    ) + #Annotate linear equation
+  annotate(
+    "text",
+    y = 365,
+    x = 0.12,
+    label = paste(expression("R²"), "=", signif(summary(model_yearly)$r.squared, digits = 3)),
+    color = "#595959",
+    size = 4,
+    hjust = 0 
+  ) +  #Annotate R2
+  
+  
+  annotate(
+    "text",
+    y = 380,
+    x = 0.12,
+    label = paste("RSME =",round(rmse,2),"lb/acre"),
+    color = "#595959",
+    size = 4,
+    hjust = 0 
+  ) + #Annotate linear equation
   
   theme_classic() +
-    theme(axis.line = element_blank(),
-          panel.border = element_rect(colour = "black", fill=NA, size=0.8),
-          legend.position = "bottom", legend.title = element_blank())
-  
-  
-  return(plot)
-  
-}
+  theme(axis.line = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=0.8),
+        legend.position = "bottom", legend.title = element_blank(),
+        panel.grid.major = element_line(color = "gray", linetype = "dashed"),
+        panel.grid.minor = element_blank()  # Hide minor gridlines
+  )
+
+ ggsave(
+  filename = "images/NDVI_ANPP_lm.png",
+ plot=last_plot(),
+ width  = 2100,
+ height = 2100,
+ units = "px",
+ bg = "white"
+ )
+ 
 
 #Saves Plot and calculates p value
 save <- function(data){
   
   plot <- ploter(data)
-  ggsave(paste0("images/NDVI_ANPP_lm_",substitute(data),".png"), plot=last_plot(), bg="white")
+  ggsave(paste0("images/NDVI_ANPP_lm.png"), plot=last_plot(), bg="white")
   
   model <- lm(NDVI ~ ANPP, data = data) %>% summary()  
   p_value <- model$coefficients[2, 4]
